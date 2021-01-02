33b15f30892e6db5910e08dbed12d85e7e303bd0
Roman Radchenko <nodenwwsfww@gmail.com>
2021-01-02 20:03:07 +0300

    remove js files

--- app/server/data/geoLocation/config.js
+++
@@ -1,2 +0,0 @@
-var URL = "https://ipgeolocation.abstractapi.com/v1/?api_key=9122b808796a40c382d98e4a6c49f319&ip_address=";
-module.exports = URL;
--- app/server/index.js
+++
@@ -1,2 +0,0 @@
-require('./events.js');
-require('./commands');
